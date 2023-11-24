d4273f02dff4254c1a2466864e00c2e3b7694e52
Joshua Schneider <68866456+VinnyTheYahtzee@users.noreply.github.com>
2023-11-24 19:35:48 +0100

    UNFUCKED

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+
+//Funktion zum Öffnen/Schließen der Tabs
 function openTab(evt, tabName) {
     //Declare all variables
     var i, tabcontent, tablinks;
@@ -16,7 +18,7 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += "active";
 }
-
+//Tag Nacht Modus
 document.getElementById('modeToggle').addEventListener('click', function () {
     let body = document.body;
     let button = this;
@@ -54,7 +56,48 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 });
+// Code zum Anzeigen/Ausblenden der inneren Tabs
+function openInnerTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
+    // Get all elements with class="inner-tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("inner-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="inner-tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("inner-tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+// Suchfunktion, derzeit ohne Funktion weil keine Datenquelle
+
+/*function searchFunction() {
+    var input, filter, results, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    results = document.getElementById("searchResults");
+
+    // Hier wird die Datenquelle festgelegt, die durchsucht werden soll.
+    // Zum Beispiel durch eine Array von Elementen gehen und mit 'filter' vergleichen.
+
+    results.innerHTML = ''; // Löscht alte Suchergebnisse
+
+    for (i = 0; i < data.length; i++) {
+        if (data[i].toUpperCase().indexOf(filter) > -1) {
+            results.innerHTML += '<p>' + data[i] + '</p>'; // Fügt passende Ergebnisse hinzu
+        }
+    }
+}*/
 
 
 
