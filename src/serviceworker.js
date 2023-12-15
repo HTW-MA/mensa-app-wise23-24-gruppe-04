c322b9f366e0d6024c187bfb9a9351090bac8f8e
AnnasHossain <annas.hossain@yahoo.de>
2023-12-15 17:22:14 +0100

    service worker_manifest

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,10 +1,53 @@
 // service-worker.js
 self.addEventListener("install", (event) => {
   console.log("Service Worker: Installiert");
-  // Hier können Sie Ressourcen cachen, wenn gewünscht
+
+  // Cache static assets
+  event.waitUntil(
+    cacheHelper.cacheAssets([
+      "/css/main.css",
+      "/js/main.js",
+      "/img/logo.png",
+    ])
+  );
 });
 
 self.addEventListener("fetch", (event) => {
   console.log("Service Worker: Fetching");
-  // Hier können Sie das Netzwerk-Request-Handling definieren
+
+  // Cache API responses
+  const response = cacheHelper.getCacheResponse(event.request);
+  if (response) {
+    event.respondWith(response);
+  } else {
+    // Fetch the resource from the network
+    event.respondWith(fetch(event.request));
+  }
 });
+
+// Hilfsfunktionen
+async function cacheHelper() {
+  // Cache-Objekt erstellen
+  const cache = await caches.open("my-cache");
+
+  // Assets zwischenspeichern
+  async function cacheAssets(assets) {
+    for (const asset of assets) {
+      await cache.add(asset);
+    }
+  }
+
+  // Cache-Response abrufen
+  async function getCacheResponse(request) {
+    const cacheEntry = await cache.match(request);
+    if (cacheEntry) {
+      return cacheEntry.response;
+    }
+  }
+
+  return {
+    cache,
+    cacheAssets,
+    getCacheResponse,
+  };
+}
