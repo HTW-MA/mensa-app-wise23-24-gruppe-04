a6d533e71bec249e846a5e604fa2d1e162ac64f7
AnnasHossain <annas.hossain@yahoo.de>
2024-01-04 22:45:37 +0100

    letzte Änderung für favicons

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true/*,
+  transpileDependencies: true,
   pwa: {
-    workboxPluginMode: 'InjectManifest', // Verwenden des InjectManifest Modus
+    name : 'Heißhunger',
+  }
+    /*workboxPluginMode: 'InjectManifest', // Verwenden des InjectManifest Modus
     workboxOptions: {
       swSrc: 'service-worker.js',
       // Weitere Workbox-Optionen hier
