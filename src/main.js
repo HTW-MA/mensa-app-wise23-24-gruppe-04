64eb820d7a8795f93af9562739371ddcdc3bfb28
AnnasHossain <annas.hossain@yahoo.de>
2024-01-20 03:59:50 +0100

    small clears

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "../router/router";
 import '../public/registerServiceWorker';
-import './manifest.json'
+import '../manifest.json'
 
 createApp(App).use(router).mount("#app");
