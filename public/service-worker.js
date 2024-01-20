42bdfdfa5423d0d170a6a51fa2493835681444f0
AnnasHossain <annas.hossain@yahoo.de>
2024-01-20 17:13:42 +0100

    ...

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,3 @@
-import {register} from "register-service-worker";
 // service-worker.js
 import { precacheAndRoute } from 'workbox-precaching';
 import * as events from "events";  // npm install workbox-precaching
@@ -50,56 +49,3 @@ self.addEventListener('sync', (event) =>{
     );
   }
 })
-
-// // Importieren der Workbox-Bibliothek
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
-//
-// // Vorauscaching der im Build-Prozess festgelegten Assets
-// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-//
-// // Caching-Strategie für Seiten-Assets (HTML, CSS, JavaScript)
-// workbox.routing.registerRoute(
-//   ({request}) => request.destination === './src/..vue' || request.destination === '/src/style.css' || request.destination === '/src/script.js',
-//   new workbox.strategies.NetworkFirst({
-//     cacheName: 'pages-cache'
-//   })
-// );
-//
-// // Caching-Strategie für Bilder
-// workbox.routing.registerRoute(
-//   ({request}) => request.destination === './public/img',
-//   new workbox.strategies.CacheFirst({
-//     cacheName: 'images-cache',
-//     plugins: [
-//       new workbox.expiration.ExpirationPlugin({
-//         maxEntries: 50,
-//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
-//       }),
-//     ],
-//   })
-// );
-//
-// // Caching-Strategie für API-Anfragen
-// workbox.routing.registerRoute(
-//   ({url}) => url.pathname.endsWith('/api/v1'),
-//   new workbox.strategies.NetworkFirst({
-//     cacheName: 'api-cache',
-//     networkTimeoutSeconds: 3,
-//   })
-// );
-//
-// self.addEventListener('activate', (event) => {
-//   console.log('Service Worker: Aktivierung und Bereinigung alter Caches');
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         cacheNames.map((cacheName) => {
-//           if (cacheName !== currentCacheName) {
-//             console.log('Service Worker: Löschen alter Cache', cacheName);
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "../router/router";
-import '../public/registerServiceWorker';
-import '../public/manifest.json'
+import '../src/registerServiceWorker';
+import '../manifest.json'
 
 createApp(App).use(router).mount("#app");
