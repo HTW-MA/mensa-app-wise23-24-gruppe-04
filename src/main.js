a4e48ebd6d851b081dff1774164bf7bb1d84f031
JaschaSteudler <jascha.steudler@bettermarks.com>
2024-01-20 17:26:16 +0100

    pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "../router/router";
-import './app'
+import '../public/app'
 
 createApp(App).use(router).mount("#app");
